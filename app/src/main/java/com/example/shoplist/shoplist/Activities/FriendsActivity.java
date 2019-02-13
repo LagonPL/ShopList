292eadc7971b8e065930afcc70dba22afdb83f75
JacekOlszewski <202256@edu.p.lodz.pl>
2019-02-13 19:35:06 +0100

    Added friend list database interaction

--- app/src/main/java/com/example/shoplist/shoplist/Activities/FriendsActivity.java
+++ app/src/main/java/com/example/shoplist/shoplist/Activities/FriendsActivity.java
@@ -1,15 +1,60 @@
 package com.example.shoplist.shoplist.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.shoplist.shoplist.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FriendsActivity extends AppCompatActivity {
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabase;
+    private String mUserId;
+
+
+    protected Button addButton;
+    protected EditText friendName;
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("friends");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mUserId = mFirebaseUser.getUid();
+
+        addButton = (Button) findViewById(R.id.AddButton);
+        friendName = (EditText) findViewById(R.id.FriendName);
+
+
+
+
+
+
+
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = friendName.getText().toString();
+                mDatabase.child("users").child(mUserId).child("friendsmail").push().setValue(name);
+            }
+        });
+
+
+
     }
+
 }
